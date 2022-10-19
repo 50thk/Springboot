7fbfe652040e0ce5506469918bdd5b3e284c283b
Bail <molrang886@gmail.com>
2022-10-19 17:11:52 +0900

    2-04 Entity

--- sbb/src/main/java/com/mysite/sbb/Answer.java
+++ sbb/src/main/java/com/mysite/sbb/Answer.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,5 +27,6 @@ public class Answer {
 
     private LocalDateTime createDate;
 
+    @ManyToOne
     private Question question;
 }
