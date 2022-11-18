8b3988ff8b71723152559967c26ad78c40d53ee6
Bail <molrang886@gmail.com>
2022-11-19 00:17:53 +0900

    2-10 질문 상세

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -16,7 +18,7 @@ public class QuestionController {
     // questionService 객체는 생성자 방식으로 DI 규칙에 의해 주입된다.
     private final QuestionService questionService;
     
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     // @ResponseBody
     // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할을 한다. Model 객체에 값을 담아두면 템플릿에서 그 값을 사용할 수 있다.
     // Model 객체는 따로 생성할 필요없이 컨트롤러 메서드의 매개변수로 지정하기만 하면 스프링부트가 자동으로 Model 객체를 생성한다.
@@ -26,4 +28,10 @@ public class QuestionController {
         return "question_list";
     }
 
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
+    }
 }
